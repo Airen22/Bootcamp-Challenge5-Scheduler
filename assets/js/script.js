8a38993f3f5fad88b947b8a5c1a7da8b8cf203d9
Erin <erinpeifer@gmail.com>
2023-01-10 22:50:19 -0600

    added function and  setInterval on militaryTime

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,32 @@
-
-var timeDisplayEl = $("#time-display");
-var timeBlockId = $(".time-block");
-var saveBtn = $(".saveBtn");
-var currentHr = dayjs().format('HH');
-
-  saveBtn.on("click", function (event){
-  var descriptionId = $(this).parent().attr("id");
-  var description = $(this).prev().val();
-  localStorage.setItem(descriptionId, description);
-})
-
-  window.onload = function(){
-    for (var i = 8; i < 21; i++) {
-      $("#hour-"+i).find("textarea").text(localStorage.getItem('hour-'+i))
-        if (currentHr == i) {
-          $("#hour-"+i).addClass("present");
-        } else if (currentHr > i) {
-          $("#hour-"+i).addClass("past");
-        } else if (currentHr < i)
-          $("#hour-"+i).addClass("future");
-        }
-      }
-  function displayTime() {
+function displayTime() {
   var rightNow = dayjs().format('MMMM DD, YYYY [at] hh:mm:ss A');
-  timeDisplayEl.text(rightNow);
-
+  $("#time-display").text(rightNow);
   }
 displayTime()
 setInterval(displayTime, 1000)
 
+function militaryTime () {
+  var currentHr = dayjs().format('HH');
+  localStorage.setItem('militaryTime', currentHr)
+}
+militaryTime()
+setInterval(militaryTime, 1000);
+
+window.onload = function(){
+  var militaryTime = localStorage.getItem('militaryTime')
+  for (var i = 8; i < 21; i++) {
+    $("#hour-"+i).find("textarea").text(localStorage.getItem('hour-'+i))
+      if ((militaryTime) == i) {
+        $("#hour-"+i).addClass("present");
+      } else if (militaryTime > i) {
+        $("#hour-"+i).addClass("past");
+      } else if (militaryTime < i)
+        $("#hour-"+i).addClass("future");
+      }
+    }
+
+$(".saveBtn").on("click", function (event){
+  var descriptionId = $(this).parent().attr("id");
+  var description = $(this).prev().val();
+  localStorage.setItem(descriptionId, description);
+})
